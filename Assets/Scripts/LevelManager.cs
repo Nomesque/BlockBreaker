6ad40d5d6d062f1f732fa273224ba12dd869fc99
Nomesque <nomesk@gmail.com>
2017-02-26 11:24:36 +1000

    Creating new brick-breaker game

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string LevelName) {
+		Debug.Log("Level requested: " + LevelName);
+		Application.LoadLevel(LevelName);
+	}
+	
+	public void LeaveGame() {
+		Debug.Log("Leave request received.");
+		Application.Quit();
+	}
+	
+}
